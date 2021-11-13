c0ce29f831651e2f87f44dad3deef8f3b58ae6b2
Anchal Bansal <anchalsingla20@gmail.com>
2021-11-12 19:02:25 -0500

    Added Validations and validation tests

--- data/songs.js
+++ data/songs.js
@@ -1,7 +1,38 @@
 const {songs} = require("../config/mongoCollection");
 const {ObjectId} = require("mongodb");
 
+function validateStringParams(param, paramName) {
+    if (!param) {
+        throw `Error: No ${paramName} passed to the function`;
+    } else if (typeof param !== "string") {
+        throw `Type Error: Argument ${param} passed is not a string ${paramName}`;
+    } else if (param.length === 0) {
+        throw `Error: No element present in string ${paramName}`;
+    } else if (!param.trim()) {
+        throw `Error: Empty spaces passed to string ${paramName}`;
+    }
+}
+
+function validateNumberParams(param, paramName) {
+    if (typeof param !== "number" || !Number.isInteger(param)) {
+        throw `Type Error: Argument ${param} passed is not a numeric ${paramName}`;
+    }
+    if (param < 0) {
+        throw `${paramName} can not be negative`;
+    }
+}
+
+function validateBoolParams(param, paramName) {
+    if (!param) {
+        throw `Error: No ${paramName} passed to the function`;
+    }
+    if (typeof param != "boolean") {
+        throw `Type Error: Argument ${param} passed is not a boolean ${paramName}`;
+    }
+}
+
 async function get(searchId) {
+    validateStringParams(searchId, "song id");
     searchId = searchId.trim();
     if (!ObjectId.isValid(searchId)) {
         throw `Error : Id passed in must be a Buffer or string of 12 bytes or a string of 24 hex characters`;
@@ -17,6 +48,26 @@ async function get(searchId) {
     return songFound;
 }
 
+function validateCreations(
+    name,
+    artist,
+    price,
+    genre,
+    description,
+    lastSoldPrice,
+    numberOfTracks,
+    currentlyAvailable
+) {
+    validateStringParams(name, "song name");
+    validateStringParams(artist, "artist name");
+    validateNumberParams(price, "Price");
+    validateStringParams(genre, "genre");
+    validateStringParams(description, "description");
+    validateNumberParams(lastSoldPrice, "last sold price");
+    validateNumberParams(numberOfTracks, "number of tracks");
+    validateBoolParams(currentlyAvailable, "currently available flag");
+}
+
 async function create(
     name,
     artist,
@@ -27,6 +78,20 @@ async function create(
     numberOfTracks,
     currentlyAvailable
 ) {
+    validateCreations(
+        name,
+        artist,
+        price,
+        genre,
+        description,
+        lastSoldPrice,
+        numberOfTracks,
+        currentlyAvailable
+    );
+    name = name.trim();
+    artist = artist.trim();
+    genre = genre.trim();
+    description = description.trim();
     const songsCollection = await songs();
     let newSong = {
         name: name,
