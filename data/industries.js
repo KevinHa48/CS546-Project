4786f9181db1a3eeb01363653a067d8f9a41ef71
KevinHa48 <kh220kh@gmail.com>
2021-11-16 00:51:52 -0500

    Small updates to industry db and wallet front end

--- data/industries.js
+++ data/industries.js
@@ -1,12 +1,40 @@
 const {industries} = require("../config/mongoCollection");
 const {ObjectId} = require("mongodb");
 
-// Industry names will be provided from an API
+// Industry names will be provided from an API.
+// These functions will only be displayed in Admin view.
+
+const idValidator = (id) => {
+    if(!id) throw "Error: No ID provided.";
+    if(id.trim().length === 0) throw "Error: Your input cannot be all whitespace";
+    
+    if(!ObjectId.isValid(id)) throw "Error: ObjectId is not of proper format.";
+    let convertedId = ObjectId(id);
+
+    return convertedId;
+}
 
 const getIndustry = async (id) => {
-    const industryCollection = await industries();
-    const foundIndustry = await industryCollection.findOne({ _id: id});
-
+    const industryId = idValidator(id);
     
+    const industryCollection = await industries();
+    const foundIndustry = await industryCollection.findOne({ _id: industryId});
+
+    if(foundIndustry === null) throw "Error: No industry with that ID.";
+    foundIndustry._id = foundIndustry._id.toString();
+
     return foundIndustry;
 }
+
+const getAllIndustries = async() => {
+    const industryCollection = await industries();
+    const allIndustries = await industryCollection.find({}).toArray();
+
+    allIndustries.forEach(industry => {
+        industry._id = industry._id.toString();
+    });
+
+    return allIndustries;
+}
+
+// For deleting and updating, the user's current portfolio would also need to be updated.
