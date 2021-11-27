e42e907a2c4d469d53b1aec99443f1e4a79db789
Marcus Zebrowski <mzebrows@stevens.edu>
2021-11-27 11:40:53 -0500

    Working on the login system for the website.
    .gitignore now includes .vscode directory.
    
    Added or beginning to add more middleware to app.js. Added cookies
    to keep track of users who have already logged in.
    
    Renamed get in data/users.js to getById and userExists to getByUsername.
    
    Added express-session to package.json
    
    Added /users routes in routes/index.js
    
    static/home.html has been converted to a handlebars file with
    home.handlebars.
    
    Created a login and signup page. Still need to work on styling
    for them.

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,15 @@
 const marketRoutes = require('./market');
 const walletRoutes = require('./wallet');
+const userRoutes = require('./users')
 const path = require('path');
 
 const constructorMethod = (app) => {
     app.use('/market', marketRoutes);
     app.use('/wallet', walletRoutes);
+    app.use('/users', userRoutes)
     app.get('/', (req, res) => {
-        res.sendFile(path.resolve('static/home.html'));
+        // res.sendFile(path.resolve('static/home.html'));
+        res.render('home', {authenticated: !!req.session.user, title: 'Beats and Blockchain'})
     });
 
     app.use('*', (req, res) => {
