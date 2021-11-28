5515909f777b65fa181e0d3b1abaf78739dbed51
NikolasStefanov <nstefano@stevens.edu>
2021-11-27 23:19:09 -0500

    CSS/Industry Routees

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,14 @@
 const marketRoutes = require('./market');
 const walletRoutes = require('./wallet');
+const industryRoutes = require('./industry');
+const stockRoutes = require('./stock');
 const path = require('path');
 
 const constructorMethod = (app) => {
     app.use('/market', marketRoutes);
     app.use('/wallet', walletRoutes);
+    app.use('/industries', industryRoutes);
+    app.use('/industry', stockRoutes)
     app.get('/', (req, res) => {
         res.sendFile(path.resolve('static/home.html'));
     });
---
+++ routes/stock.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/:id', async (req, res) => {
+    //Stuff
+    res.render('extras/stock', {
+
+    })
+})
+
+module.exports = router
