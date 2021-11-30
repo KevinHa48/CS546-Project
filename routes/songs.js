61389770fe99dd66662830e5c3f135f36faefc89
Anchal Bansal <anchalsingla20@gmail.com>
2021-11-30 12:42:44 -0500

    Remove unecessary code

--- routes/songs.js
+++ routes/songs.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const songsData = require("../data/songs");
 const router = express.Router();
-const {ObjectId} = require("mongodb");
 
 router.get("/", async (req, res) => {
     let songsList = await songsData.getAll();
