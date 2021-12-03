f3c6e6acd412c4b30702ff6a7e98032c5e55a2a9
KevinHa48 <kh220kh@gmail.com>
2021-12-02 19:06:43 -0500

    Added chart.js

--- routes/wallet.js
+++ routes/wallet.js
@@ -5,16 +5,22 @@ const router = express.Router();
 router.get("/", async (req, res) => {
     try {
         const username = req.session.user;
-        const userData = await users.getByUsername();
+        const userData = await users.getByUsername(username);
 
         console.log(userData);
 
         res.render("extras/wallet", {
             username: username,
-        })
+            stocks: userData.wallet.holdings.stocks,
+            songs: userData.wallet.holdings.songs,
+            balance: userData.wallet.balance,
+            portfolioValues: userData.wallet.portfolioValues,
+            transactions: userData.wallet.transactions
+        });
     }
     catch {
         // Error here.
+        res.status(400).json({"error": "e"})
     }
 });
 
